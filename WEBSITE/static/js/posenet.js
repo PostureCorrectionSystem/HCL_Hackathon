8a9af6ecde0f6b6e5694036e4ce0a4c12a015f35
Anantha Krishna <ananthakrishna2001@gmail.com>
2020-08-09 22:12:50 +0530

    Add files via upload

--- WEBSITE/static/js/posenet.js
+++ WEBSITE/static/js/posenet.js
@@ -21,6 +21,7 @@ const ref2 = Database1.ref("/Right/Pitch/Pitch");
 const ref3 = Database1.ref("/Right/Status");
 const ref4 = Database1.ref("/Calibration")
 const ref5 = Database1.ref("/Hunch")
+const ref6 = Database1.ref("/PastData")
 
 /**
  * Camera setup. Need to add option to use product by switching off camera(Using camera but not displaying person)
@@ -131,6 +132,7 @@ function getNoseAngle() {
  */
 
 function run() {
+
   poseNet.on("pose", (results) => {
     poses = results;
   });
@@ -139,15 +141,26 @@ function run() {
       currentAngle = getNoseAngle();
       console.log(snapshot.val(), currentAngle);
       if (Math.abs(currentAngle - threshold) > 10 || snapshot.val() == "NO") {
-
+        var currentTime = new Date();
+        var Data1 = {
+          Time: String(currentTime),
+          Posture: "YES"
+        }
         console.log("Sit properly");
         ref5.set({test:"YES"})
+        ref6.push(Data1)
         Push.create("Sit Properly!", {
           body: "PCS web notification!",
           timeout: 2000,
         });
       } else {
+        var currentTime = new Date();
+        var Data2 = {
+          Time: String(currentTime),
+          Posture: "NO"
+        }
         ref5.set({test:"NO"})
+        ref6.push(Data2)
         console.log("Okay");
       }
     });
